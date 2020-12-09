d1328f0050e9209ce4210536544143c63ca2d649
gilvan <g.r.almeida@live.com>
2020-12-09 19:36:04 -0300

    Lançando eventos de domínio

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent message, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(message.AggregateId);
+        
+            // Enviar um email para aquisição de mais produtos.
+        }
+    }
+}

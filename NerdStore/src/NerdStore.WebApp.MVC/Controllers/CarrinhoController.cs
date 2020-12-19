5a1342adadedd3ac3bb618c879736d71cdec38ba
gilvan <g.r.almeida@live.com>
2020-12-19 20:43:47 -0300

    Integração de BC's - Processamento do pedido

--- NerdStore/src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ NerdStore/src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -5,6 +5,7 @@ using NerdStore.Core.Communication.Mediator;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
 using NerdStore.Vendas.Application.Queries;
+using NerdStore.Vendas.Application.Queries.ViewModels;
 using System;
 using System.Threading.Tasks;
 
@@ -108,6 +109,39 @@ namespace NerdStore.WebApp.MVC.Controllers
             }
 
             return View(nameof(Index), await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
+
+        [HttpGet]
+        [Route("resumo-da-compra")]
+        public async Task<IActionResult> ResumoDaCompra()
+        {
+            return View(await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
+
+        [HttpPost]
+        [Route("iniciar-pedido")]
+        public async Task<IActionResult> IniciarPedido(CarrinhoViewModel carrinhoViewModel)
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+
+            var command = new IniciarPedidoCommand(
+                carrinho.PedidoId,
+                ClienteId,
+                carrinho.ValorTotal,
+                carrinhoViewModel.Pagamento.NomeCartao,
+                carrinhoViewModel.Pagamento.NumeroCartao,
+                carrinhoViewModel.Pagamento.ExpiracaoCartao,
+                carrinhoViewModel.Pagamento.CvvCartao
+            );
+
+            await _mediatorHandler.EnviarComando(command);
+
+            if (OperacaoValida())
+            {
+                return RedirectToAction("Index", "Pedido");
+            }
+
+            return View(nameof(ResumoDaCompra), await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
         }
     }
 }
